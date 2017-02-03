d78aed9eb087adf98735fd5f03c1e8efa3e908ef
Pierre Rinder <pierre.rinder@gmail.com>
2017-02-03 19:00:37 +0100

    title change

--- ui.R
+++ ui.R
@@ -10,7 +10,7 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Geocodage"),
+  titlePanel("BAM"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
